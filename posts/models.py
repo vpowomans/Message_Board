2632ae8d911fd9761e9abcd60ef598049b648eed
Iris Lizzet Urbano Ramos <iris@MBP-de-Iris.lan>
2024-11-09 15:12:31 -0600

    initial commit

---
+++ posts/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Post(models.Model):  # new
+    text = models.TextField()
+
+    def __str__(self):  # new
+        return self.text[:50]
+
+
+# Create your models here.
